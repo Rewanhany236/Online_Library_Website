61c0be8de20c65fee7830ce337a05d32494d3196
Malak <malakgamal77419@gmail.com>
2026-03-28 22:56:07 -0700

    Add some notes, modifications

---
+++ books-data.js
@@ -0,0 +1,15 @@
+
+const books = [
+  {
+    id: 1,
+    title: "Clean Code",
+    author: "Robert Martin",
+    category: "Programming.",
+    description: "Clean Code by Robert C. Martin is an essential guide for software developers " +
+    "focusing on the principles and practices of writing clean, maintainable, and efficient code.",
+    imageUrl: "images/clean-code.jpg",               
+    authorWikipedia: "https://en.wikipedia.org/wiki/Robert_C._Martin"
+  },
+  
+ 
+];
